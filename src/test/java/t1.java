cf92fb9883a3101b6a86d0f6632cf898e09c24b7
atab <atabatyr@gmail.com>
2021-02-11 21:56:29 -0600

    Architech development Asia

--- src/test/java/t1.java
+++ src/test/java/t1.java
@@ -1,3 +1,18 @@
 public class t1 {
     //it is me American Guy
+
+    @Test
+    public void element5(){
+
+    }
+    @Test
+    public void element6(){
+
+    }
+    @Test
+    public void element7(){
+
+    }
+
+
 }
