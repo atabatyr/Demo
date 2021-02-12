b7cdb2a432063baf89f54e4e9b3cc540ec8316dd
atab <atabatyr@gmail.com>
2021-02-11 22:02:14 -0600

    American guy arhitechture master

--- src/test/java/t1.java
+++ src/test/java/t1.java
@@ -1,3 +1,22 @@
 public class t1 {
     //it is me American Guy
+@Test
+    public void element(){
+
+}
+
+    @Test
+    public void element1(){
+
+    }
+    @Test
+    public void element2(){
+
+    }
+    @Test
+    public void element3(){
+
+    }
+
+
 }
